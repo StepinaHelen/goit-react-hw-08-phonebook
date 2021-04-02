3ba936a0de732f33ffef7521c8da154d9431815d
Olena <stopinahelen@gmail.com>
2021-04-02 22:26:52 +0300

    add suspense and lazy

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Container from './Components/Container';
 import AppBar from './Components/AppBar';
-import ContactsPage from './views/ContactsPage';
-import HomePage from './views/HomePage';
-import LoginPage from './views/LoginPage';
-import RegistrationPage from './views/RegistrationPage';
+// import ContactsPage from './views/ContactsPage';
+// import HomePage from './views/HomePage';
+// import LoginPage from './views/LoginPage';
+// import RegistrationPage from './views/RegistrationPage';
 import authOperation from './redux/Auth/auth-operation'
 import { connect } from 'react-redux';
 
@@ -18,7 +18,18 @@ import styles from './App.module.css'
 import PrivateRoute from './Components/PrivateRoute'
 import PublicRoute from './Components/PublicRoute'
 
-
+const HomePage = lazy(() =>
+  import('./views/HomePage.js' /* webpackChunkName: "Home-page" */),
+);
+const ContactsPage = lazy(() =>
+  import('./views/ContactsPage.js' /* webpackChunkName: "Contacts-page" */),
+);
+const LoginPage = lazy(() =>
+  import('./views/LoginPage' /* webpackChunkName: "Login-page" */),
+);
+const RegistrationPage = lazy(() =>
+  import('./views/RegistrationPage.js' /* webpackChunkName: "Registration-page" */),
+);
 class App extends React.Component{
   componentDidMount() {
     this.props.onGetCurrentUser();
@@ -32,12 +43,16 @@ class App extends React.Component{
         <div id="demo" className={styles.demo}>
         </div>
         <Container>
-      <ToastContainer />
-      <AppBar />
+          <ToastContainer />
+          <Suspense fallback={<Spinner />}>
+          <AppBar />
+          <Switch>
       <Route exact path={routes.home} component={HomePage} />
           <PrivateRoute exact path={routes.contacts} component={ContactsPage} redirectTo='/login/'/>
       <PublicRoute exact path={routes.login} component={LoginPage} restricted  redirectTo='/contacts/'/>
-          <PublicRoute exact path={routes.registration} component={RegistrationPage} restricted  redirectTo='/contacts/'/>
+            <PublicRoute exact path={routes.registration} component={RegistrationPage} restricted redirectTo='/contacts/' />
+            </Switch>
+            </Suspense>
           </Container>
          </div>
       //  {/* </Container> */}
